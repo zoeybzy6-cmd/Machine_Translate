5c1dfe9218a730abdd651162d541608936dbe851
root <zoeybzy6@gmail.com>
2025-12-28 16:04:49 +0000

    first commit

--- scripts/transformer.sh
+++ scripts/transformer.sh
@@ -3,7 +3,7 @@
 # python -u main.py --mode train --model transformer --dataset_size 100k --hid_dim 512 --n_layers 3 --n_heads 8 --pf_dim 2048 --epochs 15 --pos_type absolute --norm_type layernorm \
 #     2>&1 | tee -a ./logs/transformer_abs_layernorm.txt
 
-python -u main.py --mode test --model transformer --dataset_size 100k --hid_dim 512 --n_layers 3 --n_heads 8 --pf_dim 2048 --epochs 15 --pos_type absolute --norm_type rmsnorm --decode_strategy beam \
+python -u main.py --mode train --model transformer --dataset_size 100k --hid_dim 512 --n_layers 3 --n_heads 8 --pf_dim 2048 --epochs 15 --pos_type absolute --norm_type rmsnorm  \
     2>&1 | tee -a ./logs/transformer_abs_rmsnorm.txt
 
 # python -u main.py --mode train --model transformer --dataset_size 100k --hid_dim 512 --n_layers 3 --n_heads 8 --pf_dim 2048 --epochs 15 --pos_type alibi --norm_type layernorm \
